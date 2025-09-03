fbe30bc3c9d2539ea1fca4d34198b8c1e51b0fb1
Antonia Stoleru <2484936@edu.vaniercollege.qc.ca>
2025-09-03 11:48:42 -0400

    Finalized analyzable interface and CourseGraded class

--- src/CourseGrades.java
+++ src/CourseGrades.java
@@ -36,6 +36,40 @@ public class CourseGrades implements Analyzable {
         return "Lab score: " + grades[0].getScore() +  "   Grade: " + grades[0].getGrade() + "/n"
                 + "Pass/Fail Exam Score: " + grades[1].getScore() + "   Grade:" + grades[1].getGrade() + "/n"
                 + "Essay Score: " + grades[2].getScore() + "   Grade" + grades[2].getGrade() + "/n"
-                + "Final Exam Score: " + grades[3].getScore() + " Grade: " + grades[3].getGrade() ;
+                + "Final Exam Score: " + grades[3].getScore() + " Grade: " + grades[3].getGrade()
+                + "Average Score: " + getAverage() + "/n"
+                + "Highest Score: " + getHighest() + "/n"
+                + "Lowest Score: " + getLowest();
+    }
+
+    @Override
+    public double getAverage() {
+        return (grades[0].getScore() + grades[1].getScore() + grades[2].getScore() + grades[3].getScore())/4;
+    }
+
+    @Override
+    public GradedActivity getHighest() {
+       GradedActivity max = grades[0];
+       
+       for (int i = 1; i < 4; i++) {
+           if (grades[i].getScore() > max.getScore()) {
+               max = grades[i];
+           }
+       }
+       
+       return max;
+    }
+
+    @Override
+    public GradedActivity getLowest() {
+        GradedActivity min = grades[0];
+       
+       for (int i = 1; i < 4; i++) {
+           if (grades[i].getScore() < min.getScore()) {
+               min = grades[i];
+           }
+       }
+       
+       return min;
     }
 }
